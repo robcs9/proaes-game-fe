1647eb1f0c57eb108016580d1994aebdc5ff6e75
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-12-16 09:16:33 -0300

    Atualizando layout base

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,7 +2,7 @@ import { type PageProps } from "$fresh/server.ts";
 
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="pt-br">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,13 +10,9 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
       </head>
-      <body class={"flex flex-col justify-between"}>
-        <main>
-          <Component />
-        </main>
-        <footer class={"flex justify-center my-5"}>
-          <a href="https://www.ufpe.br/proaes" target={"_blank"}><p>&copy; 2024 PROAES UFPE</p></a>
-        </footer>
+      {/* <body class={"flex flex-col justify-between"}> */}
+      <body>
+        <Component />
       </body>
     </html>
   );
--- routes/index.tsx
+++ routes/index.tsx
@@ -9,44 +9,64 @@ import { Button } from "../components/Button.tsx";
 //   dialog.current.open()
 // }
 const handleClick = () => {
-  
-}
+};
 export default function Home() {
-  const myAPIKey = useSignal(Deno.env.get('GEOAPIFY_API_KEY'));
-  
+  // const myAPIKey = useSignal(Deno.env.get("GEOAPIFY_API_KEY"));
+
   return (
-    <div class={""}>
-      <header class={"banner flex justify-center"}>
-      <a class={"flex items-center"} href={"/"}>
+    <div class={"flex flex-col justify-between"}>
+      
+      <header class={"flex justify-center"}>
+        <a class={"flex items-center"} href={"/"}>
           <img src="/assets/logo.png" class={"w-1/4"} alt="logo" />
-        {/* <a class={""} href={"/"}> */}
-          <h1 class="px-1 text-4xl my-2 black-ops-one-regular text-center">GUIA DE ACOMODAÇÕES<br/>PARA MORADIA ESTUDANTIL</h1>
-        {/* </a> */}
+          {/* <a class={""} href={"/"}> */}
+          <h1 class="px-1 text-4xl my-2 black-ops-one-regular text-center">
+            GUIA DE ACOMODAÇÕES<br />PARA MORADIA ESTUDANTIL
+          </h1>
+          {/* </a> */}
         </a>
       </header>
-      <div class="mx-auto flex flex-col items-center justify-center">
+      
+      <main class="mx-auto flex flex-col items-center justify-center">
         <div class="flex flex-col justify-center items-center mx-20 roboto-light">
           <ul>
             <li>
-              <p className="text-xl text-justify">Está chegando à UFPE e não sabe onde morar?</p>
+              <p className="text-xl text-justify">
+                Está chegando à UFPE e não sabe onde morar?
+              </p>
             </li>
             <li>
-              <p className="text-xl text-justify">Conseguiu a tão sonhada aprovação no SISU, calouro?</p>
+              <p className="text-xl text-justify">
+                Conseguiu a tão sonhada aprovação no SISU, calouro?
+              </p>
             </li>
             <li>
-              <p className="text-xl text-justify">Está vindo em intercâmbio ou mobilidade, visitante?</p>
+              <p className="text-xl text-justify">
+                Está vindo em intercâmbio ou mobilidade, visitante?
+              </p>
             </li>
             <li>
-              <p className="text-xl text-justify">Procurando um novo lugar para morar, vetereno?</p>
+              <p className="text-xl text-justify">
+                Procurando um novo lugar para morar, vetereno?
+              </p>
             </li>
           </ul>
         </div>
-        <h3 class={"text-xl my-6 font-semibold roboto-bold-italic text-center"}>A UFPE, por meio da Proaes, preparou este GAME para te ajudar! Desejamos sucesso na busca!! <br/>Bem-vindos e bem-vindas!!!</h3>
-        
+        <h3 class={"text-xl my-6 font-semibold roboto-bold-italic text-center"}>
+          A UFPE, por meio da Proaes, preparou este GAME para te ajudar!
+          Desejamos sucesso na busca!! <br />Bem-vindos e bem-vindas!!!
+        </h3>
+
         <Map className="rounded-lg w-full" />
         {/* <Map className="h-10" myAPIKey={myAPIKey}/> */}
-
-      </div>
+      </main>
+      
+      <footer class={"flex justify-center my-5"}>
+        <a href="https://www.ufpe.br/proaes" target={"_blank"}>
+          <p>&copy; 2024 PROAES UFPE</p>
+        </a>
+      </footer>
+      
     </div>
   );
 }
