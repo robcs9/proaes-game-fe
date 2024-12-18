14599e9833c494a06750aa342ae6bfec082433a3
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-12-18 11:27:45 -0300

    Atualizando styles

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,8 +7,11 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>GAME - Guia de Acomodações para Moradia Estudantil</title>
+        {/* Flash Of Unstyled Content fix not working */}
+        {/* <link rel="preload" href="/styles.css" as={"style"}/> */}
+
         <link rel="stylesheet" href="/styles.css" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       {/* <body class={"flex flex-col justify-between"}> */}
       <body class={""}>
--- routes/index.tsx
+++ routes/index.tsx
@@ -11,7 +11,7 @@ import { Button } from "../components/Button.tsx";
 const handleClick = () => {
 };
 export default function Home() {
-  const mainList = [
+  const list = [
     "Está chegando à UFPE e não sabe onde morar?",
     "Conseguiu a tão sonhada aprovação no SISU, calouro?",
     "Está vindo em intercâmbio ou mobilidade, visitante?",
@@ -20,11 +20,12 @@ export default function Home() {
 
   return (
     <div class={"min-h-screen flex flex-col items-center"}>
-      <header class={" text-white flex justify-center p-2"}>
+      <header class={"text-white p-2"}>
+      {/* <header class={"text-white p-2 lg:w-[90%]"}> */}
       {/* <header class={"bg-blue-500 text-white flex justify-center p-2"}> */}
         <a class={"inline-block"} href={"/"}>
-          <div class={"flex gap-2 items-center border border-[#58d2f4] px-2 rounded-md"}>
-            <img src="/assets/logo.png" class="w-12" alt="logo" />
+          <div class={"flex gap-2 items-center border border-[#58d2f4] px-2 rounded-full"}>
+            <img src="/assets/logo.png" class="w-16" alt="logo" />
             <h1 class="text-xs text-nowrap text-center black-ops-one-regular">
               GUIA DE <span class={"text-[#ff0763]"}>ACOMODAÇÕES</span> PARA<br />MORADIA ESTUDANTIL
             </h1>
@@ -33,45 +34,27 @@ export default function Home() {
       </header>
 
       {/* <main class="container mx-auto p-2 flex-grow"> */}
-      <main class="container p-4 flex flex-col flex-grow gap-4 justify-evenly items-center w-[90%] rounded-md border border-violet-400">
+      <main class="container p-4 flex flex-col flex-grow gap-4 justify-center items-center w-[90%] rounded-md border border-violet-400 overflow-clip">
         {/* <div class="flex flex-col justify-center items-center mx-20 roboto-light"> */}
-        <div class="justify-center items-center roboto-light">
+        <div class="flex flex-col justify-center items-center roboto-light">
           <ul>
-            {mainList.map((elem, idx) => (
+            {list.map((elem, idx) => (
               <li key={`main-li-${idx}`}>
-                <h2 className="text-md">
+                <h2 className="text-md text-center">
                   {elem}
                 </h2>
               </li>
             ))}
           </ul>
-        <h2 class={"py-2 text-sm font-semibold roboto-bold-italic text-justify"}>
+        <h2 class={"py-2 text-md font-semibold roboto-bold-italic text-center"}>
           A UFPE, por meio da Proaes, preparou este GAME para te ajudar!
           Desejamos sucesso na busca e sejam bem-vindos e bem-vindas!!!
         </h2>
         </div>
 
-        <div class={"w-[90vw] h-[60vh]"}>
+        <div class={"w-[90vw] h-[70vh]"}>
           <Map className="w-[100%] h-[100%]" />
         </div>
-        {/* <div class={"grid grid-cols-1 sm:grid-cols-2 gap-4"}> */}
-          {/* <div class={""}> */}
-            {/* <Map className="w-dvw h-[50dvh] rounded-lg shadow-lg" /> */}
-          {/* </div> */}
-
-          {
-            /* <div class="bg-white p-6 shadow-md rounded">
-            <h2 class="text-xl mb-2">Card 7</h2>
-            <p>Some content here...</p>
-          </div> */
-          }
-          {
-            /* <div class="bg-white p-6 shadow-md rounded">
-            <h2 class="text-xl mb-2">Card 6</h2>
-            <p>Some content here...</p>
-          </div> */
-          }
-        {/* </div> */}
       </main>
 
       <footer class={"text-white p-2 mt-auto"}>
